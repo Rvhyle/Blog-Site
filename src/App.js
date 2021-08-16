9beb703974850375b7ff6ffd73010aa691d46a0a
RYLE <urciaryle@gmail.com>
2021-08-16 10:30:44 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import './stylesheets/homepage_styles.css';
+import { useEffect, useState } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import HomePage from './Pages/HomePage/HomePage';
+import BlogPage from './Pages/Blog Page/BlogPage';
+import NavLogo from './components/NavLogo';
+import getBlogs from './Helpers/fetchBlogs';
+import Loading from './components/Loading';
 
 function App() {
+  let [blogs, setBlogs] = useState([]);
+  let [fetching, setFetching] = useState(true);
+
+  useEffect(() => {
+    let blogCollection = getBlogs();
+    // Allows loading Animation to play
+    setTimeout(() => {
+      setBlogs(blogCollection);
+      setFetching(false);
+    }, 800);
+
+    console.log(blogs);
+  }, [fetching]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <NavLogo></NavLogo>
+      {fetching ? (
+        <Loading />
+      ) : (
+        <Switch>
+          <Route exact path='/'>
+            <HomePage blogs={blogs} />
+          </Route>
+          <Route exact path='/blog/:id'>
+            <BlogPage blogs={blogs} />
+          </Route>
+        </Switch>
+      )}
     </div>
   );
 }
